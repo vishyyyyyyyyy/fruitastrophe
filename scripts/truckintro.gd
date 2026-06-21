08a4c67e547e80e5588a4fb6e08a2638fa06f149
vishyyyyyyyyy <217375979+vishyyyyyyyyy@users.noreply.github.com>
2026-06-21 13:20:07 -0500

    title screen animations

--- scripts/truckintro.gd
+++ scripts/truckintro.gd
@@ -2,3 +2,20 @@ extends Node2D
 func _ready() -> void:
 	await $AnimationPlayer.animation_finished
 	$AnimationPlayer2.play("door")
+
+
+func _on_play_button_pressed() -> void:
+	$AnimationPlayer2.play("zoom cam")
+	$Camera2D2.make_current()
+
+
+func _on_load_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_settings_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
